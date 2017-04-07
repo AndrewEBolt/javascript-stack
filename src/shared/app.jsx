2e559215af9761a6c7d1dfcd6f80f9c7428d3ac0
Andrew Bolt <andrewebolt@gmail.com>
2017-04-07 16:18:52 +0930

    adding server side rendering and react-helmet for meta tags

--- src/shared/app.jsx
+++ src/shared/app.jsx
@@ -3,22 +3,23 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import { Switch } from 'react-router'
+import Helmet from 'react-helmet'
 
 import HelloAsyncPage from './component/page/hello-async'
 import HelloPage from './component/page/hello'
 import HomePage from './component/page/home'
 import Nav from './component/nav'
 import NotFoundPage from './component/page/not-found'
-import { APP_NAME } from '../shared/config'
+import { APP_NAME } from './config'
 import {
 	HELLO_PAGE_ROUTE,
 	HELLO_ASYNC_PAGE_ROUTE,
 	HOME_PAGE_ROUTE,
-} from '../shared/routes'
+} from './routes'
 
 const App = () =>
 	<div>
-		<h1>{APP_NAME}</h1>
+		<Helmet titleTemplate={`%s | ${APP_NAME}`} defaultTitle={APP_NAME} />
 		<Nav />
 		<Switch>
 			<Route exact path={HOME_PAGE_ROUTE} render={() => <HomePage />} />
